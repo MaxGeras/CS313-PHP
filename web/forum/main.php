880c48b98a2ddac8ca2130eea3ab3ad46a86e050
MaxGeras <ger14009@byui.edu>
2017-02-01 00:44:54 -0700

    my forum Postgress

--- web/forum/main.php
+++ web/forum/main.php
@@ -45,10 +45,7 @@ $db = get_db();
   
 <div class="container-fluid text-center">    
   <div class="row content">
-    <div class="col-sm-2 sidenav">
-      <p><a href="#">Link</a></p>
-      <p><a href="#">Link</a></p>
-      <p><a href="#">Link</a></p>
+    <div class="myLink">
     </div>
     <div class="col-sm-8 text-left"> 
       <h1>Welcome</h1>
