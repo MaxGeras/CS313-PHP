28ecc16da04bb195b2c2532585e2ac67aa89be79
MaxGeras <ger14009@byui.edu>
2017-02-02 12:07:37 -0700

    my forum Postgress

--- web/forum/info.php
+++ web/forum/info.php
@@ -1,8 +1,28 @@
  <?php
   session_start(); // Starting Session
 
-  require "connect.php";
-  $db = get_db();
+  function insertUserInfo($name, $pass, $email, $address, $firstname, $lastname) {
+        // prepare statement for insert
+        $sql = 'INSERT INTO user (user_name, user_password, user_email, user_address, user_firstname, user_lastname)
+         VALUES (:user_name, :user_password, :user_email, :user_address, :user_firstname, :user_lastname)';
+        $stmt = $this->pdo->prepare($sql);
+        
+        // pass values to the statement
+        $stmt->bindValue(':user_name', $name);
+        $stmt->bindValue(':user_password', $pass);
+        $stmt->bindValue(':user_email', $email);
+        $stmt->bindValue(':user_address', $address);
+        $stmt->bindValue(':user_firstname', $firstname;
+        $stmt->bindValue(':user_name', $lastname);
+        
+        // execute the insert statement
+        $stmt->execute();
+        
+        // return generated id
+        return $this->pdo->lastInsertId('user_id_seq');
+    }
+
+
 
   $userName = pg_escape_string($_POST['uname']);
   $pass = pg_escape_string($_POST['psw']);
@@ -11,21 +31,27 @@
   $fname = pg_escape_string($_POST['fname']);
   $lname = pg_escape_string($_POST['lname']);
 
-  $query = " INSERT INTO user (id, user_name, user_password, user_email, user_address, user_firstname, user_lastname)
-  VALUES ( 1, '" .$userName. "','" .$pass. "','" .$email. "','" .$address. "', '" .$fname. "', '" .$lname. "')";
+
+
+try {
+    // connect to the PostgreSQL database
+   require "connect.php";
+   $db = get_db();
+ 
+    // insert a stock into the stocks table
+    $id = insertUserInfo( $userName, $pass, $email, $address, $fname, $lname);
+    echo 'The stock has been inserted with the id ' . $id . '<br>';
+ 
+} 
+catch (\PDOException $e) {
+    echo $e->getMessage();
+}
   
-  $result = pg_query($db, $query); 
-
-  if (!$result) { 
-           pg_last_error($db);  
-           exit(); 
-   }
-
-  pg_close();
  
   $_SESSION["login_user"] = $userName; // Initializing Session
   $_SESSION["pass_user"] = $pass;
 
-  header('location: main.php'); 
+  //header('location: main.php'); 
+  //die();
   
-  ?>
+?>
