37e5f187a5e2b849013a0ffcf27c5ad34a15cdd5
MaxGeras <ger14009@byui.edu>
2017-02-01 22:50:40 -0700

    my forum Postgress

--- web/forum/validate.php
+++ web/forum/validate.php
@@ -1,7 +1,7 @@
 <?php
 session_start(); // Starting Session
 
-if (!isset($_POST['login']) || strlen(trim($_POST['login'])) == 0 || !isset($_POST['password']) 
+if (!isset($_POST['login']) || strlen(trim($_POST['login'])) == 0 || !isset($_POST['password'])) 
 {
 	header('location: login.php'); // Redirecting To Other Page
 	exit();
@@ -17,9 +17,8 @@ else
 	$username = htmlspecialchars($_POST['login']);
 	$password = htmlspecialchars($_POST['password']);
 
-	$stmt = $db->prepare('SELECT * FROM user WHERE user_name=:user_name AND user_password=:user_password');
+	$stmt = $db->prepare('SELECT * FROM user WHERE user_name=:user_name');
 	$stmt->bindValue(':user_name', $username, PDO::PARAM_STR);
-	$stmt->bindValue(':user_password', $password, PDO::PARAM_STR);
 	$stmt->execute();
 	$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
     
