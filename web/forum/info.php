c39c3c3d719a0aee2bb5b82fe6d7e8f217d3b942
MaxGeras <ger14009@byui.edu>
2017-02-01 23:28:33 -0700

    my forum Postgress

--- web/forum/info.php
+++ web/forum/info.php
@@ -5,13 +5,17 @@
    $db = get_db();
 
 
-    $query = "INSERT INTO user(user_name,user_password,user_email,user_address,user_firstName,user_lastName)
-    VALUES ('$_POST[uname]','$_POST[psw]','$_POST[email]','$_POST[address]', '$_POST[fname]', '$_POST[lname]')";
-    $result = pg_query($query);
+    $result = pg_query($db, "INSERT INTO user(user_name, user_password, user_email, user_address, user_firstName, user_lastName)
+    VALUES ('$_POST['uname']','$_POST['psw']','$_POST['email']','$_POST['address']', '$_POST['fname']', '$_POST['lname']');");
+    
 
 
     $username = htmlspecialchars($_POST['uname']);
     $password = htmlspecialchars($_POST['psw']);
+
+    echo $username;
+    echo $password;
+
     $_SESSION["login_user"] = $username; // Initializing Session
     $_SESSION["pass_user"] = $username;
 
